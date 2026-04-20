60b3555672a86d77b8695b5da2561305857b0236
Aiden Styers <aiden.styers@gmail.com>
2026-04-19 19:05:47 -0500

    Fixed some bugs in previous merge. First implementation of receive game results.

--- Backend/db_management.py
+++ Backend/db_management.py
@@ -1,19 +1,20 @@
 import sqlite3
 import os
 
-DB_PATH = os.getenv("DATABASE_PATH", "/app/sqlitedb/Game_Info.db")
+DB_PATH = os.getenv("DATABASE_PATH", "/app/sqlitedb/Game_Data.db")
+
+# Ensure the directory exists
+os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
 
 def initialize():
-    # Ensure the directory exists
-    os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
-    
+
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     
     # Use 'IF NOT EXISTS' to make the script idempotent
     cursor.execute("""
-        CREATE TABLE IF NOT EXISTS game_info (
-            table_name VARCHAR(100) PRIMARY KEY,
+        CREATE TABLE IF NOT EXISTS game_data (
+            name_of_table VARCHAR(100) PRIMARY KEY,
             in_1 INTEGER,
             in_2 INTEGER,
             in_3 INTEGER,
@@ -23,7 +24,7 @@ def initialize():
             in_7 INTEGER, 
             in_8 INTEGER,
             in_9 INTEGER,
-            in_10 INTEGER      s
+            in_10 INTEGER      
             )"""
         )
     
--- Backend/main.py
+++ Backend/main.py
@@ -1,16 +1,38 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 import asyncio
+from db_management import *
 
 app = FastAPI()
 
+current_table_name = "test_table"
+
 @app.post("/game-results")
 async def receive_game_results(guesses: int):
-    return {
-        "status": "success",
-        "guesses_received": guesses,
-        "result_message": f"You made {guesses} guesses!"
-    }
+    global current_table_name, DB_PATH
+
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    print(cursor.execute("SELECT COUNT(*) FROM game_data WHERE name_of_table = '" + current_table_name + "'").fetchall())
+
+    if cursor.execute("SELECT COUNT(*) FROM game_data WHERE name_of_table = '" + current_table_name + "'").fetchall()[0][0] == 0:
+        print("Test: " + "INSERT INTO game_data VALUES ('" + current_table_name + "'" + (", 0"*10) + ")")
+        cursor.execute("INSERT INTO game_data VALUES ('" + current_table_name + "'" + (", 0"*10) + ")")
+    
+    if guesses < 10:
+        cursor.execute("UPDATE game_data SET in_" + str(guesses) + " = in_" + str(guesses) + " + 1 WHERE name_of_table = '" + current_table_name + "'")
+    else:
+        cursor.execute("UPDATE game_data SET in_10 = in_10 + 1 WHERE name_of_table = '" + current_table_name + "'")
+
+    resp = cursor.execute("SELECT * FROM game_data WHERE name_of_table = '" + current_table_name + "'").fetchall()[0]
+
+    conn.commit()
+    conn.close()
+
+    return {"status": "success", "global_guesses": resp}
+
+
 
 # Simple HTML page for testing WebSocket
 html = """
