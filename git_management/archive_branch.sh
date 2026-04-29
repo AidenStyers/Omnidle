df5f6a8f1dc10c45a2454d1e814b527b8f21a7ec
Aiden Styers <aiden.styers@gmail.com>
2026-04-29 15:51:06 -0500

    Created script to archive branches, use with care

---
+++ git_management/archive_branch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "VITAL: Make sure all of your local changes are committed! Otherwise only some of the commands will run. CTRL-C right now and check!!"
+
+read -p "Enter the name of the branch to archive: " BRANCH_NAME
+
+git checkout $BRANCH_NAME
+git tag archive/$BRANCH_NAME
+git push origin archive/$BRANCH_NAME --no-verify
+git checkout main
+git branch -d $BRANCH_NAME          # Local delete
+git push origin --delete $BRANCH_NAME --no-verify # Remote delete
+
+echo "$BRANCH_NAME archived!"
