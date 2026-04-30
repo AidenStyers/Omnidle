a768a5a6e2a8307093fd7f7638604d767025c1b8
Aiden Styers <aiden.styers@gmail.com>
2026-04-30 17:48:55 -0500

    Test commit 2

--- git_management/unarchive_branch.sh
+++ git_management/unarchive_branch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "VITAL: Make sure all of your local changes are committed! Otherwise only some of the commands will run. CTRL-C right now and check!!"
+echo "Make sure all of your local changes are committed! Otherwise the script will fail. CTRL-C right now and check!!"
 
 read -p "Enter the name of the branch to un-archive: " BRANCH_NAME
 
